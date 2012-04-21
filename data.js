8a17b92ea26791995e7526cc83371b38f2fa8f0e
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2012-04-22 00:21:56 +0300

    add birds. add selector UI

--- data.js
+++ data.js
@@ -1,4 +1,8 @@
-var images = [
+
+var media = {};
+
+media.animals = {};
+media.animals.images = [
 "http://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Coq_Belle-Ile.jpg/1024px-Coq_Belle-Ile.jpg",
 "http://upload.wikimedia.org/wikipedia/commons/c/c4/Lleyn_sheep.jpg",
 "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Sus_scrofa_scrofa.jpg/1280px-Sus_scrofa_scrofa.jpg",
@@ -9,8 +13,7 @@ var images = [
 "http://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/European_shorthair_procumbent_Quincy.jpg/1280px-European_shorthair_procumbent_Quincy.jpg",
 "http://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Chickens_eating.jpg/1024px-Chickens_eating.jpg"
 ];
-
-var sounds = [
+media.animals.sounds = [
 "http://www.animal-sounds.org/farm/Rooster%20animals070.wav",
 "http://www.animal-sounds.org/farm/Sheep%20animals058.wav",
 "http://www.animal-sounds.org/farm/Pigs%20eating%20animals113.wav",
@@ -20,4 +23,28 @@ var sounds = [
 "http://www.animal-sounds.org/farm/Duck-quacking%20animals038.wav",
 "http://www.animal-sounds.org/farm/Cat%20meow%20animals021.wav",
 "http://www.animal-sounds.org/farm/Chicken%20coop%20animals050.wav"
+];
+
+media.birds = {};
+media.birds.images = [
+"http://upload.wikimedia.org/wikipedia/commons/7/7b/Nachtigall_%28Luscinia_megarhynchos%29.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Parus_major_2_Luc_Viatour.jpg/1280px-Parus_major_2_Luc_Viatour.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Chaffinch_%28Fringilla_coelebs%29.jpg/1280px-Chaffinch_%28Fringilla_coelebs%29.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/b/b0/Cuculus_canorus_vogelartinfo_chris_romeiks_CHR0791.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Uhu-muc.jpg/1205px-Uhu-muc.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Gallinago_gallinago_a1.JPG/1092px-Gallinago_gallinago_a1.JPG",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Gull_ca_usa.jpg/1280px-Gull_ca_usa.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Wrona_Siwa.jpg/1280px-Wrona_Siwa.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Wood.pigeon.slimbridge.arp.jpg/1280px-Wood.pigeon.slimbridge.arp.jpg"
+];
+media.birds.sounds = [
+"http://upload.wikimedia.org/wikipedia/commons/a/a9/XN_Luscinia_megarhynchos_012.ogg",
+"http://upload.wikimedia.org/wikipedia/commons/e/eb/Parus_major_song.ogg",
+"http://upload.wikimedia.org/wikipedia/commons/a/a1/Fringilla_coelebs_short.ogg",
+"http://upload.wikimedia.org/wikipedia/commons/0/01/Cuculus_canorus.ogg",
+"http://www.animal-sounds.org/Air/owl%20animals074.wav",
+"http://upload.wikimedia.org/wikipedia/commons/2/24/Gallinago_gallinago.ogg",
+"http://www.animal-sounds.org/Air/Seagulls%20animals133.wav",
+"http://upload.wikimedia.org/wikipedia/commons/9/91/Corvus_cornix.ogg",
+"http://upload.wikimedia.org/wikipedia/commons/1/12/Columba_palumbus_birdsong.ogg"
 ];
