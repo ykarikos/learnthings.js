31fe1c6ff24eda2a795ed2d56b4d4e48ba6632dc
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2012-04-21 23:02:25 +0300

    use src attribute instead of source elemnt for audio. move media urls to data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+var images = [
+"http://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Coq_Belle-Ile.jpg/1024px-Coq_Belle-Ile.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/c/c4/Lleyn_sheep.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Sus_scrofa_scrofa.jpg/1280px-Sus_scrofa_scrofa.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Cow_female_black_white.jpg/1280px-Cow_female_black_white.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/2/26/YellowLabradorLooking_new.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Nokota_Horses.jpg/1280px-Nokota_Horses.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Anas_platyrhynchos_var._domesticus_01.jpg/1280px-Anas_platyrhynchos_var._domesticus_01.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/European_shorthair_procumbent_Quincy.jpg/1280px-European_shorthair_procumbent_Quincy.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Chickens_eating.jpg/1024px-Chickens_eating.jpg"
+];
+
+var sounds = [
+"http://www.animal-sounds.org/farm/Rooster%20animals070.wav",
+"http://www.animal-sounds.org/farm/Sheep%20animals058.wav",
+"http://www.animal-sounds.org/farm/Pigs%20eating%20animals113.wav",
+"http://www.animal-sounds.org/farm/Cow%20animals084.wav",
+"http://www.animal-sounds.org/farm/Dog%20bark%20animals079.wav",
+"http://www.animal-sounds.org/farm/Horse-whinnying%20animals037.wav",
+"http://www.animal-sounds.org/farm/Duck-quacking%20animals038.wav",
+"http://www.animal-sounds.org/farm/Cat%20meow%20animals021.wav",
+"http://www.animal-sounds.org/farm/Chicken%20coop%20animals050.wav"
+];
